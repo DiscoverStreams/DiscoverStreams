e48581b5d2a7687157f25617d272d77f807af816
Misty Porter <misty.blue17@gmail.com>
2021-11-05 10:24:21 -0600

    Updates with new study sites

--- DataRetrieval.R
+++ DataRetrieval.R
@@ -12,21 +12,153 @@ oname <- paste(wname,"outputs",sep="/") # will open outputs file in workikng dir
 pname<-paste(wname,"plots",sep="/") # will open plots file in workikng directory
 fname<-paste(wname,"functions",sep="/") # will open functions file in workikng directory
 
-streamflow_monthly_hamilton <- read_csv("~/GradSchool/Data/Streamflow_Monthly_NWIS/20170626/Syracuse_MonthlyStreamflow_1902-2017.csv")
-groundwater_monthly_hamilton <- read_csv("~/GradSchool/Data/GW_Level_WIZARD/Hamilton_AnnualGWLevel_1939-2017.csv")
-pdsi_annual_climdiv7 <- read_delim("~/GradSchool/Data/PDSI_NOAA/2017/ClimDiv7_Monthly_20173112.txt", delim = ", ")
-wateruse_irrigation_annual_hamilton <- read_csv("~/GradSchool/Data/Pumping_WIMAS/2017/Hamilton_AnnualWaterUseIrrigation_1957-2016.csv")
+CSVcolNames <- c("Year", "Month", "Value", "JSON")
+CSVtoJSON <- read_excel("~/GradSchool/DiscoverWaterLive/CSVtoJSON_DW.xlsx", col_names = CSVcolNames)
+CSVtoJSON <- CSVtoJSON[ , -(3:4)]
 
---------------------- IMPORT & PREPARE DATA -------------------------
-q_select <- streamflow_monthly_hamilton[-(1) , c(2, 5:7)] %>% 
-  filter(year_nu > 1949)
-gw_select <- groundwater_monthly_hamilton[ , c(1,4)] %>% 
-  filter(Year > 1949)
+pdsi_annual_climdiv7 <- read_delim("~/GradSchool/Data/PDSI_NOAA/2017/ClimDiv7_Monthly_20173112.txt", delim = ", ")
+
+
+groundwater_winter_barton <- read_excel("~/GradSchool/Data/GW_Level_WIZARD/20200915/Barton_GW_AnnualAvg.xlsx", sheet = "WinterAverages", skip = 2)
+groundwater_winter_finney <- read_excel("~/GradSchool/Data/GW_Level_WIZARD/20200915/Finney_GW_AnnualAvg.xlsx", sheet = "WinterAverages", skip = 2)
+groundwater_winter_ford <- read_excel("~/GradSchool/Data/GW_Level_WIZARD/20200915/Ford_GW_AnnualAvg.xlsx", sheet = "WinterAverages", skip = 2)
+groundwater_winter_hamilton <- read_excel("~/GradSchool/Data/GW_Level_WIZARD/20200915/Hamilton_GW_AnnualAvg.xlsx", sheet = "WinterAverages", skip = 2)
+groundwater_winter_sedgwick <- read_excel("~/GradSchool/Data/GW_Level_WIZARD/20200915/Sedgwick_GW_AnnualAvg.xlsx", sheet = "WinterAverages", skip = 2)
+
+irrigation_barton <- read_excel("~/GradSchool/Data/Pumping_WIMAS/20200915/Barton_WaterUseIrrigateation_AnnualSummary.xlsx")
+irrigation_finney <- read_excel("~/GradSchool/Data/Pumping_WIMAS/20200915/Finney_WaterUseIrrigateation_AnnualSummary.xlsx")
+irrigation_ford <- read_excel("~/GradSchool/Data/Pumping_WIMAS/20200915/Ford_WaterUseIrrigateation_AnnualSummary.xlsx")
+irrigation_hamilton <- read_excel("~/GradSchool/Data/Pumping_WIMAS/20200915/Hamilton_WaterUseIrrigateation_AnnualSummary.xlsx")
+irrigation_sedgwick <- read_excel("~/GradSchool/Data/Pumping_WIMAS/20200915/Sedgwick_WaterUseIrrigateation_AnnualSummary.xlsx")
+
+streamflow_monthly_syracuse <- read_tsv("~/GradSchool/Data/Streamflow_Monthly_NWIS/20200915/7138000_Syracuse_Qmo.csv", skip =35, col_types = cols(agency_cd = "c", site_no = "d", parameter_cd = "d", ts_id = "d", year_nu = "d", month_nu = "d", mean_va = "d"))
+spec(streamflow_monthly_syracuse)
+streamflow_monthly_dodgecity <- read_tsv("~/GradSchool/Data/Streamflow_Monthly_NWIS/20200915/7139500_DodgeCity_Qmo.csv", skip =35, col_types = cols(agency_cd = "c", site_no = "d", parameter_cd = "d", ts_id = "d", year_nu = "d", month_nu = "d", mean_va = "d"))
+spec(streamflow_monthly_dodgecity)
+streamflow_monthly_gardencity <- read_tsv("~/GradSchool/Data/Streamflow_Monthly_NWIS/20200915/7139500_GardenCity_Qmo.csv", skip =35, col_types = cols(agency_cd = "c", site_no = "d", parameter_cd = "d", ts_id = "d", year_nu = "d", month_nu = "d", mean_va = "d"))
+spec(streamflow_monthly_gardencity)
+streamflow_monthly_greatbend <- read_tsv("~/GradSchool/Data/Streamflow_Monthly_NWIS/20200915/7141300_GreatBend_Qmo.csv", skip =35, col_types = cols(agency_cd = "c", site_no = "d", parameter_cd = "d", ts_id = "d", year_nu = "d", month_nu = "d", mean_va = "d"))
+spec(streamflow_monthly_greatbend)
+streamflow_monthly_wichita <- read_tsv("~/GradSchool/Data/Streamflow_Monthly_NWIS/20200915/7144300_Wichita_Qmo.csv", skip =35, col_types = cols(agency_cd = "c", site_no = "d", parameter_cd = "d", ts_id = "d", year_nu = "d", month_nu = "d", mean_va = "d"))
+spec(streamflow_monthly_wichita)
+
+# --------------------- IMPORT & PREPARE DATA -------------------------
+
 Year <- gsub("(.{6}.)", "\\1 ", pdsi_annual_climdiv7$`   YearMonth`)[[1]]
 pdsi_select <- pdsi_annual_climdiv7[ , ]
 
-wuirr_select <- wateruse_annual_hamilton[ , c(1,2)]
-wuirr_select$`Diverted (ft^3/s)` <- (wateruse_annual_hamilton$`Total Acre-Feet Diverted` * 43559.935)/(1*3.154e+7)
-wuirr_select <- wuirr_select[ , -(2)]
 
+### GROUNDWATER (WIZARD) ###
+data_join <- left_join(CSVtoJSON, groundwater_winter_barton, by = "Year")
 
+gw_winter_barton <- data_join[ , c(1,2,5)]
+write.csv(gw_winter_barton, "~/GradSchool/Data/GW_Level_WIZARD/20200120/Barton_GW_WinterAvg.csv", row.names = FALSE, na = "null")
+
+data_join <- left_join(CSVtoJSON, groundwater_winter_finney, by = "Year")
+
+gw_winter_finney <- data_join[ , c(1,2,5)]
+write.csv(gw_winter_finney, "~/GradSchool/Data/GW_Level_WIZARD/20200120/Finney_GW_WinterAvg.csv", row.names = FALSE, na = "null")
+
+data_join <- left_join(CSVtoJSON, groundwater_winter_ford, by = "Year")
+
+gw_winter_ford <- data_join[ , c(1,2,5)]
+write.csv(gw_winter_ford, "~/GradSchool/Data/GW_Level_WIZARD/20200120/Ford_GW_WinterAvg.csv", row.names = FALSE, na = "null")
+
+data_join <- left_join(CSVtoJSON, groundwater_winter_hamilton, by = "Year")
+
+gw_winter_hamilton <- data_join[ , c(1,2,5)]
+write.csv(gw_winter_hamilton, "~/GradSchool/Data/GW_Level_WIZARD/20200120/Hamilton_GW_WinterAvg.csv", row.names = FALSE, na = "null")
+
+data_join <- left_join(CSVtoJSON, groundwater_winter_sedgwick, by = "Year")
+
+gw_winter_sedgwick <- data_join[ , c(1,2,5)]
+write.csv(gw_winter_sedgwick, "~/GradSchool/Data/GW_Level_WIZARD/20200120/Sedgwick_GW_WinterAvg.csv", row.names = FALSE, na = "null")
+
+### IRRIGATION (WIMAS) ###
+wuirr_select <- irrigation_barton
+wuirr_select$`Diverted (ft^3/s)` <- round(((irrigation_barton$`Total Acre-Feet Diverted` * 43559.935)/31536000), 2)  # convert volume diverted in acre-feet/yr to cubic feet/yr then convert cubic feet/yr to cubic feet/s, round result to 2 places
+
+data_join <- left_join(CSVtoJSON, wuirr_select, by = "Year")
+
+wuirr_barton <- data_join[ , c(1,2,5)]
+write.csv(wuirr_barton, "~/GradSchool/Data/Pumping_WIMAS/20200915/Barton_WaterUseIrrigateation_AnnualSummary.csv", row.names = FALSE, na = "null")
+
+wuirr_select <- irrigation_finney
+wuirr_select$`Diverted (ft^3/s)` <- round(((irrigation_finney$`Total Acre-Feet Diverted` * 43559.935)/31536000), 2)  # convert volume diverted in acre-feet/yr to cubic feet/yr then convert cubic feet/yr to cubic feet/s, round result to 2 places
+
+data_join <- left_join(CSVtoJSON, wuirr_select, by = "Year")
+
+wuirr_finney <- data_join[ , c(1,2,5)]
+write.csv(wuirr_finney, "~/GradSchool/Data/Pumping_WIMAS/20200915/Finney_WaterUseIrrigateation_AnnualSummary.csv", row.names = FALSE, na = "null")
+
+wuirr_select <- irrigation_ford
+wuirr_select$`Diverted (ft^3/s)` <- round(((irrigation_ford$`Total Acre-Feet Diverted` * 43559.935)/31536000), 2)  # convert volume diverted in acre-feet/yr to cubic feet/yr then convert cubic feet/yr to cubic feet/s, round result to 2 places
+
+data_join <- left_join(CSVtoJSON, wuirr_select, by = "Year")
+
+wuirr_ford <- data_join[ , c(1,2,5)]
+write.csv(wuirr_ford, "~/GradSchool/Data/Pumping_WIMAS/20200915/Ford_WaterUseIrrigateation_AnnualSummary.csv", row.names = FALSE, na = "null")
+
+wuirr_select <- irrigation_hamilton
+wuirr_select$`Diverted (ft^3/s)` <- round(((irrigation_hamilton$`Total Acre-Feet Diverted` * 43559.935)/31536000), 2)  # convert volume diverted in acre-feet/yr to cubic feet/yr then convert cubic feet/yr to cubic feet/s, round result to 2 places
+
+data_join <- left_join(CSVtoJSON, wuirr_select, by = "Year")
+
+wuirr_hamilton <- data_join[ , c(1,2,7)]
+write.csv(wuirr_hamilton, "~/GradSchool/Data/Pumping_WIMAS/20200915/Hamilton_WaterUseIrrigateation_AnnualSummary.csv", row.names = FALSE, na = "null")
+
+wuirr_select <- irrigation_sedgwick
+wuirr_select$`Diverted (ft^3/s)` <- round(((irrigation_sedgwick$`Total Acre-Feet Diverted` * 43559.935)/31536000), 2)  # convert volume diverted in acre-feet/yr to cubic feet/yr then convert cubic feet/yr to cubic feet/s, round result to 2 places
+
+data_join <- left_join(CSVtoJSON, wuirr_select, by = "Year")
+
+wuirr_sedgwick <- data_join[ , c(1,2,5)]
+write.csv(wuirr_sedgwick, "~/GradSchool/Data/Pumping_WIMAS/20200915/Sedgwick_WaterUseIrrigateation_AnnualSummary.csv", row.names = FALSE, na = "null")
+
+### STREAMFLOW (NWIS) ###
+qColNames = c("Year", "Month", "Discharge")
+
+q_select <- streamflow_monthly_syracuse[-(1) , c(5:7)] %>% 
+  filter(year_nu > 1949)
+colnames(q_select) = qColNames
+
+data_join <- left_join(CSVtoJSON, q_select)
+
+q_syracuse <- data_join
+write.csv(q_syracuse, "~/GradSchool/Data/Streamflow_Monthly_NWIS/20200915/Syracuse_Qmo.csv", row.names = FALSE, na = "null")
+
+q_select <- streamflow_monthly_gardencity[-(1) , c(5:7)] %>% 
+  filter(year_nu > 1949)
+colnames(q_select) = qColNames
+
+data_join <- left_join(CSVtoJSON, q_select)
+
+q_gardencity <- data_join
+write.csv(q_gardencity, "~/GradSchool/Data/Streamflow_Monthly_NWIS/20200915/GardenCity_Qmo.csv", row.names = FALSE, na = "null")
+
+q_select <- streamflow_monthly_dodgecity[-(1) , c(5:7)] %>% 
+  filter(year_nu > 1949)
+colnames(q_select) = qColNames
+
+data_join <- left_join(CSVtoJSON, q_select)
+
+q_dodgecity <- data_join
+write.csv(q_dodgecity, "~/GradSchool/Data/Streamflow_Monthly_NWIS/20200915/DodgeCity_Qmo.csv", row.names = FALSE, na = "null")
+
+q_select <- streamflow_monthly_greatbend[-(1) , c(5:7)] %>% 
+  filter(year_nu > 1949)
+colnames(q_select) = qColNames
+
+data_join <- left_join(CSVtoJSON, q_select)
+
+q_greatbend <- data_join
+write.csv(q_greatbend, "~/GradSchool/Data/Streamflow_Monthly_NWIS/20200915/GreatBend_Qmo.csv", row.names = FALSE, na = "null")
+
+q_select <- streamflow_monthly_wichita[-(1) , c(5:7)] %>% 
+  filter(year_nu > 1949)
+colnames(q_select) = qColNames
+
+data_join <- left_join(CSVtoJSON, q_select)
+
+q_wichita <- data_join
+write.csv(q_wichita, "~/GradSchool/Data/Streamflow_Monthly_NWIS/20200915/Wichita_Qmo.csv", row.names = FALSE, na = "null")
---
+++ DataRetrieval_StreamflowNWIS_v2.R
@@ -0,0 +1,253 @@
+library(readr)
+library(ggplot2)
+library(ggthemes)
+library(tidyverse)
+library(DataExplorer)
+library(dplyr)
+library(jsonlite)
+library(jsonify)
+library(dataRetrieval)
+
+
+setwd("~/GradSchool/DiscoverStreams")
+wname <- getwd() # set back to working directory
+dname <- paste(wname,"data",sep="/") # will open data file in workikng directory
+oname <- paste(wname,"outputs",sep="/") # will open outputs file in workikng directory
+pname<-paste(wname,"plots",sep="/") # will open plots file in workikng directory
+
+
+
+### STREAMFLOW (NWIS) - MICHIGAN ###
+site_number <- c("04097540", "04096900", "04101500")
+site_info <- readNWISsite(site_number)
+color_mi <- c("darkorchid", "black", "deepskyblue")
+
+### Prairie River near Nottawa ###
+site_number <- c("04097540")
+# view(Cheney_info)
+parameter_code <- c("00060")
+parameter_names <- c("Discharge, cubic feet per second")
+start_date <- ""
+end_date <- ""
+
+raw_daily_Prairie1 <- readNWISdv(site_number, parameter_code, start_date, end_date)
+
+raw_daily_Prairie1 <- drop_columns(raw_daily_Prairie1, c("agency_cd", "X_00060_00003_cd"))
+
+colnames(raw_daily_Prairie1) <- c("SiteNum", "Date", "Discharge_cfs")
+
+### Nottawa Creek - feeds Prairie River ###
+site_number <- c("04096900")
+
+raw_daily_Prairie2 <- readNWISdv(site_number, parameter_code, start_date, end_date)
+
+raw_daily_Prairie2 <- drop_columns(raw_daily_Prairie2, c("agency_cd", "X_00060_00003_cd"))
+
+colnames(raw_daily_Prairie2) <- c("SiteNum", "Date", "Discharge_cfs")
+
+### St. Joseph River ###
+site_number <- c("04101500")
+raw_daily_StJoseph <- readNWISdv(site_number, parameter_code, start_date, end_date)
+
+raw_daily_StJoseph <- drop_columns(raw_daily_StJoseph, c("agency_cd", "X_00060_00003_cd"))
+
+colnames(raw_daily_StJoseph) <- c("SiteNum", "Date", "Discharge_cfs")
+
+### DataExplorer - Plot Streamflow ###
+# DataExplorer::create_report(raw_daily_USGS)
+
+# x <- raw_daily_USGS$Date
+# y <- raw_daily_USGS$Discharge_cfs
+# z <- raw_daily_USGS$SiteNum
+
+
+plot(raw_daily_USGS$Date, raw_daily_USGS$Discharge_cfs,
+     type = "l", 
+     # main = "Michigan", 
+     xlab = "Date", 
+     ylab = "Discharge (Cfs)",
+     # log = "x,y",
+     col = color_sv[4])
+# lines(raw_daily_Prairie2$Date, raw_daily_Prairie2$Discharge_cfs,
+#       type = "l",
+#       col = "darkorchid")
+# lines(raw_daily_Prairie1$Date, raw_daily_Prairie1$Discharge_cfs, 
+#       type = "l",
+#       col = "deepskyblue")
+legend("topleft", 
+       legend = site_info$station_nm, 
+       lty = 1:1, 
+       col = color_sv[4], 
+       # col = c("darkorchid", "deepskyblue", "black"), 
+       title = "Station",
+       cex = 0.6)
+
+
+
+
+### STREAMFLOW (NWIS) - SCOTT VALLEY, CALIFORNIA ###
+site_number <- c("11519500", "11519000", "11518200", "11518050")
+site_info <- readNWISsite(site_number)
+color_sv <- c("aquamarine3", "darkblue", "darkred", "burlywood3")
+
+
+
+### STREAMFLOW (NWIS) - KAWEAH, CALIFORNIA ###
+site_number <- c("11209500", "11208600", "11208000", "11208730", "11208615", "11206820")
+site_info <- readNWISsite(site_number)
+color_kv <- c("darkorchid", "black", "deepskyblue", "chartreuse4", "coral2")
+
+### Kaweah R at Kaweah ###
+site_number <- c("11518050")   
+site_name <- site_info$station_nm
+
+raw_daily_USGS <- readNWISdv(site_number, parameter_code, start_date, end_date)
+
+raw_daily_USGS <- drop_columns(raw_daily_USGS, c("agency_cd", "X_00060_00003_cd"))
+
+colnames(raw_daily_USGS) <- c("SiteNum", "Date", "Discharge_cfs")
+
+daily_streamflow_CA <- raw_daily_USGS
+# view(daily_streamflow_CA)
+
+
+### Kaweah R Below Conduit 2 Near Hammond ###
+site_number <- c("11208600")
+site_name <- site_info$station_nm
+
+raw_daily_USGS <- readNWISdv(site_number, parameter_code, start_date, end_date)
+
+raw_daily_USGS <- drop_columns(raw_daily_USGS, c("agency_cd", "site_no", "X_00060_00003_cd"))
+
+colnames(raw_daily_USGS) <- c("Date", site_name[1])
+
+# daily_streamflow_CA <- left_join(daily_streamflow_CA, raw_daily_USGS, by "Date")
+# view(daily_streamflow_CA)
+
+
+### Kaweah R Marble Fork ###
+site_number <- c("11208000")
+site_name <- site_info$station_nm
+
+raw_daily_USGS <- readNWISdv(site_number, parameter_code, start_date, end_date)
+
+raw_daily_USGS <- drop_columns(raw_daily_USGS, c("agency_cd", "site_no", "X_00060_00003_cd"))
+
+colnames(raw_daily_USGS) <- c("Date", site_name[1])
+
+daily_streamflow_CA <- left_join(daily_streamflow_CA, raw_daily_USGS)
+# view(daily_streamflow_CA)
+
+
+### EF Kaweah R Near Three Rivers ###
+site_number <- c("11208730")   
+site_name <- site_info$station_nm
+
+raw_daily_USGS <- readNWISdv(site_number, parameter_code, start_date, end_date)
+print(raw_daily_USGS [raw_daily_USGS == 0.00])
+raw_daily_USGS <- drop_columns(raw_daily_USGS, c("agency_cd", "site_no", "X_00060_00003_cd"))
+
+raw_daily_USGS$X_00060_00003 [raw_daily_USGS$X_00060_00003 == 0.00] <- 0.001
+
+colnames(raw_daily_USGS) <- c("Date", site_name[1])
+
+
+daily_streamflow_CA <- left_join(daily_streamflow_CA, raw_daily_USGS)
+# view(daily_streamflow_CA)
+
+
+### EF Kaweah R Below Monarch Near Hamond ---> NO DATA!!! ###
+site_number <- c("11208615")   
+site_name <- site_info$station_nm
+
+raw_daily_USGS <- readNWISdv(site_number, parameter_code, start_date, end_date)
+
+raw_daily_USGS <- drop_columns(raw_daily_USGS, c("agency_cd", "site_no", "X_00060_00003_cd"))
+
+colnames(raw_daily_USGS) <- c("Date", site_name[1])
+
+daily_streamflow_CA <- left_join(daily_streamflow_CA, raw_daily_USGS)
+# view(daily_streamflow_CA)
+
+
+### Kaweah R Marble Fork Above Horse C Near Lodge Pole ###
+site_number <- c("11206820")   
+site_name <- site_info$station_nm
+
+raw_daily_USGS <- readNWISdv(site_number, parameter_code, start_date, end_date)
+
+raw_daily_USGS <- drop_columns(raw_daily_USGS, c("agency_cd", "site_no", "X_00060_00003_cd"))
+
+colnames(raw_daily_USGS) <- c("Date", site_name[1])
+
+daily_streamflow_CA <- left_join(daily_streamflow_CA, raw_daily_USGS)
+view(daily_streamflow_CA)
+
+
+x <- raw_daily_USGS$Date
+y <- raw_daily_USGS$Discharge_cfs
+z <- raw_daily_USGS$SiteNum
+
+plot(x, y, 
+     type = "l", 
+     main = "Michigan", 
+     xlab = "Date", 
+     ylab = "Discharge (Cfs)",
+     col = ifelse(raw_daily_USGS$SiteNum == "04096900","darkorchid", 
+                  ifelse(raw_daily_USGS$SiteNum == "04097540","deepskyblue", 
+                         "grey")))
+legend("topleft", legend = site_info$station_nm, lty = 1:1, col = c("darkorchid", "deepskyblue"), title = "Stations", cex = 0.6)
+
+smoothScatter(raw_daily_USGS$Date, raw_daily_USGS$Discharge_cfs, main = site_info$station_nm, xlab = "Date", ylab = "Discharge (Cfs)")
+
+
+
+
+
+########
+# qColNames = c("Year", "Month", "Discharge")
+# 
+# q_select <- streamflow_monthly_syracuse[-(1) , c(5:7)] %>% 
+#   filter(year_nu > 1949)
+# colnames(q_select) = qColNames
+# 
+# data_join <- left_join(CSVtoJSON, q_select)
+# 
+# q_syracuse <- data_join
+# write.csv(q_syracuse, "~/GradSchool/Data/Streamflow_Monthly_NWIS/20200915/Syracuse_Qmo.csv", row.names = FALSE, na = "null")
+# 
+# q_select <- streamflow_monthly_gardencity[-(1) , c(5:7)] %>% 
+#   filter(year_nu > 1949)
+# colnames(q_select) = qColNames
+# 
+# data_join <- left_join(CSVtoJSON, q_select)
+# 
+# q_gardencity <- data_join
+# write.csv(q_gardencity, "~/GradSchool/Data/Streamflow_Monthly_NWIS/20200915/GardenCity_Qmo.csv", row.names = FALSE, na = "null")
+# 
+# q_select <- streamflow_monthly_dodgecity[-(1) , c(5:7)] %>% 
+#   filter(year_nu > 1949)
+# colnames(q_select) = qColNames
+# 
+# data_join <- left_join(CSVtoJSON, q_select)
+# 
+# q_dodgecity <- data_join
+# write.csv(q_dodgecity, "~/GradSchool/Data/Streamflow_Monthly_NWIS/20200915/DodgeCity_Qmo.csv", row.names = FALSE, na = "null")
+# 
+# q_select <- streamflow_monthly_greatbend[-(1) , c(5:7)] %>% 
+#   filter(year_nu > 1949)
+# colnames(q_select) = qColNames
+# 
+# data_join <- left_join(CSVtoJSON, q_select)
+# 
+# q_greatbend <- data_join
+# write.csv(q_greatbend, "~/GradSchool/Data/Streamflow_Monthly_NWIS/20200915/GreatBend_Qmo.csv", row.names = FALSE, na = "null")
+# 
+# q_select <- streamflow_monthly_wichita[-(1) , c(5:7)] %>% 
+#   filter(year_nu > 1949)
+# colnames(q_select) = qColNames
+# 
+# data_join <- left_join(CSVtoJSON, q_select)
+# 
+# q_wichita <- data_join
+# write.csv(q_wichita, "~/GradSchool/Data/Streamflow_Monthly_NWIS/20200915/Wichita_Qmo.csv", row.names = FALSE, na = "null")
